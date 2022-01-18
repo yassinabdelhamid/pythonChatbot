2e0193e571093f5d080a23823993f3e090c7292e
Yassin Abdelhamid <yassin1001@gmail.com>
2022-01-18 20:32:13 +0100

    Added function check message

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ def message_probability(user_message, recognised_words, single_response=False, r
     # Calculates the percent of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
 
+    # Checks that required words are in the string
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else:
+        return 0
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+
 def get_response(unser_input):
     split_message = re.split(r'\s+|[,;?!.-]\s*', unser_input.lower())
     response = check_all_messages(split_message)
