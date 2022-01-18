102f5a9668b0f4f09b22e9b632ae994223357ea7
Yassin Abdelhamid <yassin1001@gmail.com>
2022-01-18 19:22:00 +0100

    Word calculator user message
    Calculates the percent of recognised words in a user message

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import re
 import long_responses as long
 
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    # Counts how many words are present in each predefined message
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
 def get_response(unser_input):
-    split_message = re.split(r'\s+|[,;?!.-]')
+    split_message = re.split(r'\s+|[,;?!.-]\s*', unser_input.lower())
+    response = check_all_messages(split_message)
+    return response
 
 # Testing response system
 while True:
